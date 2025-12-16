6b527069aa68a5bb38bc6ccb538378010524ac28
Nam Pham <nam@tokamak.network>
2025-12-16 12:26:20 +0700

    chore: update comment

--- src/features/integrations/components/ComponentsTab.tsx
+++ src/features/integrations/components/ComponentsTab.tsx
@@ -262,8 +262,8 @@ export function ComponentsTab({ stack }: RollupDetailTabProps) {
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
             {integrations
               .filter((integration) =>
-                // Show active integrations: Completed, InProgress, Pending
-                // and hide: failed, Cancelled, Cancelling, terminated, Stopped 
+                // Show active integrations: Completed, InProgress, Pending, Cancelling
+                // and hide: failed, Cancelled, terminated, Stopped 
                 integration.status === "Completed" ||
                 integration.status === "InProgress" ||
                 integration.status === "Pending" ||
